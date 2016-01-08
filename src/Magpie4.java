a64445c5700d6f67ba75fce8329b348845817811
ethanadamsm <ethnadamsm@gmail.com>
2016-01-08 10:22:46 -0700

    Code started

--- src/Magpie4.java
+++ src/Magpie4.java
@@ -47,6 +47,10 @@ public class Magpie4 {
 		else if (findKeyword(statement, "I want to", 0) >= 0) {
 			response = transformIWantToStatement(statement);
 		}
+		
+		else if (findKeyword(statement, "I want", 0) >= 0){
+			response = transfromIWantSomethingStatement(statement);
+		}
 
 		else {
 			// Look for a two word (you <something> me)
@@ -80,6 +84,18 @@ public class Magpie4 {
 		int psn = findKeyword(statement, "I want to", 0);
 		String restOfStatement = statement.substring(psn + 9).trim();
 		return "What would it mean to " + restOfStatement + "?";
+	}
+	
+	private String transfromIWantSomethingStatement(String statement){
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if(lastChar.equals(".")){
+			statement = statement.substring(0,  statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you be really happy if you had " + restOfStatement + "?";
+		
 	}
 
 	/**
